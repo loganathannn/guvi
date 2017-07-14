21fc807a9ef7a2eb45ac62bf86d510d0baeaff36
loganathannn <loguhari09@gmail.com>
2017-07-14 14:46:42 +0530

    Create Sumodd.java

---
+++ Sumodd.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Sumodd {
+public static void main(String[] args)
+{ 
+	int c=0,d=0;
+	int n=15;
+	for(int i=1;i<=n;i++)
+	{
+		c=c+i;
+	}
+	System.out.println("sum of values"+c);
+for(int j=15;j<=45;j++)
+{
+	if(j%2==0)
+	{
+	
+	}
+	else
+	{
+	d=d+j;
+	}
+	
+}
+System.out.println("Sum of odd number"+d);
+	
+}
+}
